6506c4c169761a8e3657d2175148c6c93bd1bbab
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2016-04-14 19:53:47 +0700

    Claw script

---
+++ Assets/Scripts/Claw.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Claw : MonoBehaviour {
+
+	public Transform Origin = null;
+	public float Speed = 4f;
+	public Turret TurretScript = null;
+
+	public int GemValue = 100;
+
+	private Vector3 target;
+	private GameObject childObject = null;
+	private LineRenderer lineRenderer;
+
+	private bool isGemHit = false;
+	private bool isRetracting = false;
+
+	void Awake() {
+		lineRenderer = GetComponent<LineRenderer> ();
+	}
+
+	void Update() {
+		float step = Speed * Time.deltaTime;
+
+		transform.position = Vector3.MoveTowards (transform.position, target, step);
+		lineRenderer.SetPosition (0, Origin.position);
+		lineRenderer.SetPosition(1, transform.position);
+
+		if (transform.position == Origin.position && isRetracting && TurretScript != null) {
+			TurretScript.CollectedObject ();
+
+			if (isGemHit) {
+
+				isGemHit = false;
+			}
+
+			if (childObject != null) {
+				Destroy (childObject);
+			}
+
+			gameObject.SetActive(false);
+			isRetracting = false;
+		}
+	}
+}
