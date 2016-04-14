0bea6005e7b743760ee77202e8511d7da991da7a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2016-04-14 18:58:26 +0700

    Incompelte turret script

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -3,13 +3,45 @@ using System.Collections;
 
 public class Turret : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public GameObject Claw = null;
+	public Animator AnimController_Gun = null;
+	private bool isShooting = false;
+	private LayerMask currentMask;
+
+	public bool IsShooting{
+		get { 
+			return isShooting;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update(){
+		if (Input.GetButtonDown ("Fire1") && !isShooting) {
+			LaunchClaw ();
+		}
+	}
+
+	private void LaunchClaw() {
+		isShooting = true;
+
+		if (AnimController_Gun != null) {
+			AnimController_Gun.speed = 0f
+		}
+
+		RaycastHit hit;
+
+		Vector3 down = transform.TransformDirection(Vector3.down);
+
+		if(Physics.Raycast(transform.position, 	down, out hit, 100f)) {
+			
+		}
+	}
+
+
+	public void CollectedObject(){
+		isShooting = false;
+
+		if (AnimController_Gun != null) {
+			AnimController_Gun.speed = 1f;
+		}
 	}
 }
